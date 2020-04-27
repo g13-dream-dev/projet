32995133eaed9de1a9678205d3c2141ac223efba
Lindsey Toukam <lind.toukam@gmail.com>
2020-04-27 10:09:25 +0200

    Création de la classe Test_TL2

--- src/test/git/Test_TL2.java
+++ src/test/git/Test_TL2.java
@@ -1,10 +1,10 @@
 package test.git;
 
-public class Test_TL {
+public class Test_TL2 {
 	
 	
 	public void bonjour() {
-		System.out.println( "Salut" );
+		System.out.println( "Bonjoour" );
 	}
 	
 	
